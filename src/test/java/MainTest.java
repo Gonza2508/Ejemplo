4a525dbca4282b914fd272c586a3124bf587cd0d
Gonzalo <llaos@fi.uba.ar>
2024-07-07 23:52:39 -0300

    Agrego test

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+    @Test
+    public void test01() {
+        int numeroEsperado = 2;
+        assertEquals(numeroEsperado, 2);
+    }
+}
